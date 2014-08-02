50adf24d3fd0f00df6cd6c0af9ebcb2dd46e2262
Brandon Kindred <Brandon.kindred@gmail.com>
2014-08-01 23:14:59 -0400

    added csrf token to xhr before dropzone sends file

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,15 @@
+$.ajaxSetup({
+  headers: {
+    'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+  }
+});
+
 Dropzone.options.myDropzone = {
   init: function() {
+    this.on("sending",function(file, xhr, formData){
+	token = $('meta[name="csrf-token"]').attr('content');
+	xhr.setRequestHeader('X-CSRF-Token', token);
+    });
     this.on("complete", function(file) { alert("Added file."); });
   }
 };
