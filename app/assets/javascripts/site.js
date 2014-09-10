8f56470edfd8f510b01ea738aa170accc5c9758a
Brandon Kindred <Brandon.kindred@gmail.com>
2014-09-09 23:23:05 -0400

    Added Masonry library for tiling the ideas on the ideas index page.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -17,3 +17,11 @@ Dropzone.options.myDropzone = {
   }
 };
 
+$(document).ready(function(){
+  var container = document.querySelector('#masonry');
+  var masonry = new Masonry(container, {
+    columnWidth: 50,
+    itemSelector: '.item'
+  });
+});
+
