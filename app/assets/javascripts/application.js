966cae6852112bb83820fc601c759b4c2e3c5bce
Brandon Kindred <Brandon.kindred@gmail.com>
2014-09-26 19:53:31 -0400

    added resource index page to the idea page as a partial. Added Jquery file tree with intent of using it to navigate file system for a given repository.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// Any JavaScript/Coffee file within this eirectory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
@@ -21,3 +21,11 @@
 //= require_tree ./angular/controllers
 //= require_tree ./angular/directives
 
+
+
+
+$(document).ready( function() {
+   $('#resource_container').fileTree({ root: '/public/repositories/' }, function(file) {
+     alert(file);
+   });
+});
