28373a2ce091d2669a8fa1bf7c6253c0a285e876
Brandon Kindred <Brandon.kindred@gmail.com>
2014-09-28 21:34:06 -0400

    added client side check for comment. If no comment is given when a file is uploaded then the request is aborted and the user is alerted of need for comment

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -13,7 +13,14 @@ Dropzone.options.myDropzone = {
       $("#resourceComment").val(comment);
     });
     this.on("sending",function(file, xhr, formData){
-      xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+      var comment = $("#resourceComment").val();
+      if(comment === ""){
+        alert("You must enter a comment in order to upload");
+        xhr.abort();
+      }
+      else{
+        xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+      }
     });
     this.on("complete", function(file) { 
       $('form.edit_idea > input#alteredStatus').val('1');
