4a0dc303b34fef6146cb9e27dd1d8f10380d5191
Brandon Kindred <brandon.kindred@gmail.com>
2015-01-26 19:28:23 -0500

    Changed ideaSerive create method to change header content

--- app/assets/javascripts/angular-app/services/ideaServices.js
+++ app/assets/javascripts/angular-app/services/ideaServices.js
@@ -3,7 +3,13 @@ var services = angular.module('ideaBin.ideaServices', ['ngResource']);
 services.factory('Idea', function ($resource) {
     return $resource('/ideas/:id.json',{id: '@id'}, {
         query: { method: 'GET', isArray: true },
-        create: { method: 'POST' },
+        create: { 
+									$http.post(uploadUrl, fd, {
+											withCredentials: true,
+											headers: {'Content-Type': undefined },
+											transformRequest: angular.identity
+									}).success( ...all right!... ).error( ..damn!... ); 
+								},
 				show: { method: 'GET', isArray: false},
         update: { method: 'PUT' },
         delete: { method: 'DELETE' }, 
