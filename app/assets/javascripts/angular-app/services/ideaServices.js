7a6953763fcfa02953fa7e4f6ad37b283f26f2db
Brandon Kindred <brandon.kindred@gmail.com>
2015-02-15 17:26:06 -0500

    added in button for creating ideas

--- app/assets/javascripts/angular-app/services/ideaServices.js
+++ app/assets/javascripts/angular-app/services/ideaServices.js
@@ -3,6 +3,7 @@ var services = angular.module('ideaBin.ideaServices', ['ngResource']);
 services.factory('Idea', function ($resource, $upload) {
     return $resource('/ideas/:id.json',{id: '@id'}, {
         query: { method: 'GET', isArray: true },
+				create: { method: 'POST' },
 				show: { method: 'GET', isArray: false},
         update: { method: 'PUT' },
         delete: { method: 'DELETE' }, 
