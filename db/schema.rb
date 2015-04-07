251114eee98a80e1feed51707e2fc0cf1dac741b
Brandon Kindred <brandon.kindred@gmail.com>
2015-04-06 20:43:47 -0400

    added in avatar attachment fields in user table. also updated paperclip gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325233214) do
+ActiveRecord::Schema.define(version: 20150407003053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,6 +143,10 @@ ActiveRecord::Schema.define(version: 20150325233214) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "admin"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
