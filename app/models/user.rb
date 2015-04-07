f195529969f74f3f86662d6d410b5dd9f95c24bd
Brandon Kindred <brandon.kindred@gmail.com>
2015-04-07 00:09:23 -0400

    all but paperclip working on windows

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :avatar
 	
-	has_attached_file :avatar, :path => ":rails_root/public/system/:attachment/:id/:basename_:style.:extension", :styles => { :medium => "300x300>", :thumb => "100x100>" }, :url => "/images/:attachment/:id/:basename_:style.:extension", :default_style => :thumb
+	has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png", :default_style => :thumb
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   # attr_accessible :title, :body
   has_many :repositories
