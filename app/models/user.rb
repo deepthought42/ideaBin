c8a070e3b5aaf3945592971973cab7f087cb9f6a
Brandon Kindred <brandon.kindred@gmail.com>
2015-04-09 00:38:29 -0400

    added omniauth gems for twitter, facebook and google

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me  
   
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+# :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
 	
 	
 	
