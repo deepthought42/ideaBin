c377f0cd2f4318670b7f6adbf33bb4f0b23531a2
Brandon Kindred <brandon.kindred@gmail.com>
2015-04-14 22:48:36 -0400

    made sessions last 1 week and timouts after 30 minutes

--- app/assets/javascripts/angular-app/controllers/ideaControllers.js
+++ app/assets/javascripts/angular-app/controllers/ideaControllers.js
@@ -5,7 +5,7 @@ app.controller("IdeaIndexController", ['$scope', '$localStorage', 'Idea', 'Repos
 		$scope.$storage = $localStorage;
 		$scope.ideas = Idea.query();
 		
-  	$scope.deleteIdea =  function(idea){
+  		$scope.deleteIdea =  function(idea){
 			Idea.delete({id: idea.id});
 			var index = $scope.ideas.indexOf(idea);
 			$scope.ideas.splice(index, 1);
@@ -63,12 +63,12 @@ app.controller("IdeaIndexController", ['$scope', '$localStorage', 'Idea', 'Repos
 		
 		$scope.showUserIdeas = function(userId){
 			$http.get("/userIdeas/" + userId+".json")
-				.success(function(data){ 
-					$scope.ideas = data;
-				})
-				.error(function(data){
-					alert(data.errors);
-				});
+			.success(function(data){ 
+				$scope.ideas = data;
+			})
+			.error(function(data){
+				alert(data.errors);
+			});
 		}
 		
 		$scope.$on('hideCreateIdeaPanel', function(event, data) {
