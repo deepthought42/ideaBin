d312e29bbd3abe6cf4569a4d0dba28936fc7174c
Brandon Kindred <brandon.kindred@gmail.com>
2015-04-24 20:28:48 -0400

    fixed up contact us form

--- app/assets/javascripts/angular-app/controllers/contactController.js
+++ app/assets/javascripts/angular-app/controllers/contactController.js
@@ -3,8 +3,10 @@ var app = angular.module('ideaBin.contactController', []);
 
 app.controller('contactController', ['$scope', 'Contact', '$location',
 	function($scope, Contact, $location ){
+		$scope.options = ['PRAISE', 'ISSUE', 'FUNC_REQUEST', 'QUESTION']
 		//callback for ng-click 'createNewResourceFactory'
 		$scope.contact = {};
+		$scope.contact.reason = $scope.options[0]
 		$scope.createContact = function(){
 			console.log($scope.contact)
 			Contact.create($scope.contact);
