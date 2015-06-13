00f5f94f1e38f3e9e327cfb2e243266f8510b3a6
Brandon Kindred <brandon.kindred@gmail.com>
2015-06-12 23:33:09 -0400

    changed location from /ideas to /ideaIndex in navigation controller

--- app/assets/javascripts/angular-app/controllers/navigationsControllers.js
+++ app/assets/javascripts/angular-app/controllers/navigationsControllers.js
@@ -5,18 +5,18 @@ app.controller("NavigationController",['$scope', '$location', '$http', '$rootSco
 		$scope.$session = $sessionStorage;
 		$scope.showMyIdeas = function(){
 			$rootScope.$broadcast('showMyIdeas', $scope.$session.user.id);
-			$location.path('/ideas')
+			$location.path('/ideaIndex')
 		}
-		
+
 		$scope.showNewIdea = function(){
 			$rootScope.$broadcast('showAddIdea');
 		}
-		
+
 		$scope.showAllIdeas = function(){
 			$rootScope.$broadcast('showAllIdeas');
-			$location.path('/ideas')
+			$location.path('/ideaIndex')
 		}
-				
+
 		$scope.showContactUs = function(){
 			$location.path('/contact_us');
 		};
