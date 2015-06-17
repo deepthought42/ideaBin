e2e5c9686cd61f2b3a52b8faa4dd368168add6fa
Brandon Kindred <brandon.kindred@gmail.com>
2015-06-16 22:53:01 -0400

    fixed bug with directory path not being fully contructed correctly

--- app/assets/javascripts/angular-app/controllers/directoryController.js
+++ app/assets/javascripts/angular-app/controllers/directoryController.js
@@ -12,26 +12,26 @@ app.controller("DirectoryIndexCtrl", ['$scope', '$rootScope', '$localStorage', '
 			console.log("PARENT DIR :: " + parent_dir_path);
 			$scope.loadDirectory("", $scope.$storage.repo.path + parent_dir_path);
 		}
-		
+
   	$scope.deleteDirectory =  function(directory){
 			Directory.delete({id: 1, path: $localStorage.repo.path + $localStorage.dir_path + directory });
 			var index = $scope.directories.indexOf(directory);
 			$scope.directories.splice(index, 1);
 		}
-		
+
 		$scope.newDirectory = function(){
 			$location.path('/directories/new');
 		}
-		
-		$scope.$on('addDirectory', function(event, data) { 
+
+		$scope.$on('addDirectory', function(event, data) {
 			$scope.directories.push(data);
 		});
-		
+
 		$scope.loadDirectory = function(name, path){
 			$scope.directories = Directory.query({'path': path+"/"+name})
 			if(typeof name != 'undefined' && name != null && name != ''){
 				if($scope.$storage.dir_path.lastIndexOf("/") === $scope.$storage.dir_path.length-1){
-					$scope.$storage.dir_path += name;
+					$scope.$storage.dir_path += name + "/";
 				}
 				else{
 					$scope.$storage.dir_path +=  name + "/";
@@ -42,10 +42,10 @@ app.controller("DirectoryIndexCtrl", ['$scope', '$rootScope', '$localStorage', '
 			}
 			$rootScope.$broadcast('loadResources', $scope.$storage.repo.path + $scope.$storage.dir_path);
 		}
-		
+
 		$scope.$on('loadDirectory', function(event, path) {
 			$scope.loadDirectory("", path)
-		});	
+		});
 }]);
 
 app.controller('DirectoryCreationCtrl', ['$scope', '$rootScope', '$localStorage', '$sessionStorage', '$routeParams', 'Directory', '$location',
@@ -62,7 +62,7 @@ app.controller('DirectoryCreationCtrl', ['$scope', '$rootScope', '$localStorage'
 			console.log("DIRECTORY NAME :: " + $scope.directoryForm.name);
 			$rootScope.$broadcast("addDirectory", $scope.directoryForm.name )
 		}
-		
+
 		$scope.hideDirectoryEditPanel = function() {
 			$rootScope.createDirectoryPanelVisible = false;
 		}
